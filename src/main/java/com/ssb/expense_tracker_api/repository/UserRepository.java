f1c5e972833b5e7f1993e040822ce68d19fb946f
Suryansh Singh Bisen <07suryansh@gmail.com>
2026-08-04 19:46:37 +0530

    create user repository

---
+++ src/main/java/com/ssb/expense_tracker_api/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.ssb.expense_tracker_api.repository;
+
+
+import com.ssb.expense_tracker_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User,UUID> {
+
+    Optional<User> findUserByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+}
